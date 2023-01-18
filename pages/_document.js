13ce34591afdd13218d8697f47fbfc7cd1ba5daf
Dominik Kopec <kopec.dominik@gmail.com>
2023-01-18 02:32:36 +0000

    fix(ui): repaired favicon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel='favicon' href='/favicon.ico'></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
